4829180b1cf336db9c2104dee3d3f7661265e0a3
thegoose20 <hello@lucyhavens.com>
2025-02-20 13:31:43 -0800

    Updating data paths

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 xml_paths = "data/xml_paths.csv"
 classified_data_path = "data/classified/"
-coded_data_path = "data/manually_coded/"
+coded_data_path1 = "data/manually_coded_part1/"
+coded_and_classified = "data/coded_and_classified"
