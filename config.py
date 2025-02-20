ef7131a89eb22ed30dfa5fb5fc2620d719a8cf9e
thegoose20 <hello@lucyhavens.com>
2025-02-20 13:41:13 -0800

    Classified last manually coded collection

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 xml_paths = "data/xml_paths.csv"
 classified_data_path = "data/classified/"
 coded_data_path1 = "data/manually_coded_part1/"
-coded_and_classified = "data/coded_and_classified"
+coded_and_classified = "data/coded_and_classified/"
